a8f2f8c86c5fb7685739edd2c08626a04e17b9d9
cc01748 <cc01748@surrey.ac.uk>
2022-07-26 15:05:47 +0100

    Login logout added for admin.

--- hikeme_app/urls.py
+++ hikeme_app/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
 from hikeme_app import views
+from django.contrib import admin
+from django.urls import include, path
+from django.views.generic.base import TemplateView
+
 
 urlpatterns = [
-    path("", views.home, name="home"),
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('admin/', admin.site.urls),
+    path("accounts/", include("django.contrib.auth.urls")),
+    
 ]
