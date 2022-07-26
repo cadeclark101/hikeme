5a87a8fb481197de41840ed14de565cfbfc6a5bf
cc01748 <cc01748@surrey.ac.uk>
2022-07-26 22:14:05 +0100

    Added Node-RED and file structure to simulate network of check-in locations

--- hikeme_app/models.py
+++ hikeme_app/models.py
@@ -1,2 +1,39 @@
+from tkinter import CASCADE
 from django.db import models
 
+class Person(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    contact_number = models.IntegerField()
+    emergency_contact_number = models.IntegerField()
+    address = models.TextField()
+    departure = models.DateTimeField()
+    eta = models.DateTimeField()
+
+class Status(models.Model):
+    person_status = models.ForeignKey(
+        Person,
+        on_delete=models.CASCADE
+    )
+    status = models.CharField(max_length=100)
+    datetime = models.DateTimeField()
+
+class Trail(models.Model):
+    person_trail = models.OneToOneField(
+        Person,
+        on_delete=models.CASCADE
+    )
+    trail_name = models.CharField(max_length=100)
+    trail_checkpoint = models.IntegerField()
+    trail_length = models.IntegerField()
+    trail_warnings = models.CharField(max_length=100)
+    
+class Authority(models.Model):
+    last_known_location = models.OneToOneField(
+        Trail,
+        on_delete=models.CASCADE
+    )
+    name = models.CharField(max_length=100)
+    county = models.CharField(max_length=100)
+    contact_number = models.IntegerField()
+    
