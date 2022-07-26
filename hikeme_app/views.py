2cd9ca781828b86ab0dda9195c3f37abf545b6ce
cc01748 <cc01748@surrey.ac.uk>
2022-07-26 17:26:50 +0100

    Added register page.

--- hikeme_app/views.py
+++ hikeme_app/views.py
@@ -1,8 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import  render, redirect
+from django.urls import is_valid_path
+from .forms import NewUserForm
+from django.contrib.auth import login
+from django.contrib import messages
 
 # Create your views here.
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Hello, DJANGO!")
+    return redirect("/")
 
+def register_request(request):
+    if request.method == "POST":
+        form = NewUserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, "Registration successful.")
+            return redirect("login")
+        messages.error(request, "Error when registering")
+    form = NewUserForm()
+    return render (request=request, template_name="registration/register.html", context={"register_form":form})
