4d614c517b36af60f8a81555b15a7db5ee506416
cc01748 <cc01748@surrey.ac.uk>
2022-07-27 18:29:48 +0100

    Added route to display JSON data stored in trail_data.js based on the trails ID.

--- hikeme_nodes/app.js
+++ hikeme_nodes/app.js
@@ -6,6 +6,8 @@ app.listen(5000, () => {
     console.log('server is listening on port 5000')
 })
 
-app.get('/', (req, res) => {
-    res.json(trail_checkpoints)
+app.get('/checkpoint/:checkpoint_id' ,(req, res) => {
+    const id = Number(req.params.checkpoint_id)
+    const trail_checkpoint = trail_checkpoints.find(trail_checkpoints => trail_checkpoints.id === id)
+    res.json(trail_checkpoint)
 })
--- hikeme_nodes/trail_data.js
+++ hikeme_nodes/trail_data.js
@@ -16,6 +16,16 @@ const trail_authority =[
         id: 1,
         authority_name: "Cleveland Mountain Rescue Team.",
         authority_contact_number: "999"
+    },
+    {
+        id: 2,
+        authority_name: "Mountain Rescue England and Wales.",
+        authority_contact_number: "999"
+    },
+    {
+        id: 3,
+        authority_name: "Surrey Search and Rescue.",
+        authority_contact_number: "999"
     }
 ]
 
@@ -25,6 +35,18 @@ const trail_infos =[
         trail_name: "Cleveland Way.",
         length: "177",
         warnings: [trail_warnings[0], trail_warnings[1]],
+    },
+    {
+        id: 2,
+        trail_name: "England Coast Path.",
+        length: "4500",
+        warnings: [trail_warnings[1]],
+    },
+    {
+        id: 3,
+        trail_name: "North Downs Way",
+        length: "246",
+        warnings: [trail_warnings[0]],
     }
 ]
 
@@ -33,6 +55,16 @@ const trail_checkpoints = [
         id: 1, 
         info: trail_infos[0],
         authority: trail_authority[0]
+    },
+    { 
+        id: 2, 
+        info: trail_infos[1],
+        authority: trail_authority[1]
+    },
+    { 
+        id: 3, 
+        info: trail_infos[2],
+        authority: trail_authority[2]
     }
 ]
 
