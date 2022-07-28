008ebe45cfde7e0fb7d84b9e1cb195f1b029b523
cadeclark101 <cadeclark@outlook.com>
2022-07-28 16:48:18 +0100

    Added views for SPA.

--- hikeme_nodes/app.js
+++ hikeme_nodes/app.js
@@ -1,8 +1,10 @@
 const express = require('express')
+var path = require('path');
 const app = express()
 const trail_checkpoints = require('./trail_data.js')
 
 app.set('view engine', 'pug');
+app.use("/public", express.static(path.join(__dirname, 'public')));
 
 app.listen(5000, () => {
     console.log('Started Hikeme Node Network Simulator on 127.0.0.1:5000')
