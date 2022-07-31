f8884a9ccf926dc3e7f6846c7bce589d8ac04f60
cadeclark101 <cadeclark@outlook.com>
2022-07-31 21:01:00 +0100

    Tried SQLite - cannot handle multple connections, only multi-threading so not good for IoT. Try MongoDB next.

--- hikeme_nodes/app.js
+++ hikeme_nodes/app.js
@@ -1,11 +1,17 @@
 const express = require('express')
-var path = require('path');
+const path = require('path');
 const app = express();
 const trail_data = require('./static/js/trail_data.js');
+const fs = require('fs');
+const sqlite3 = require('sqlite3');
 
 app.set('view engine', 'pug');
+
 app.use("/static", express.static('./static/'));
 app.use("/public", express.static(path.join(__dirname, 'public')));
+
+
+
 
 app.listen(5000, () => {
     console.log('Started Hikeme Node Network Simulator on 127.0.0.1:5000')
@@ -14,3 +20,5 @@ app.listen(5000, () => {
 app.get("/hikemenodes", (req, res) => {
     res.render('index');
 });
+
+
