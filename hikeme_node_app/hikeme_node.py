80d45af2a4efdd8b1055c1a48beb20dff441b14e
cadeclark101 <cadeclark@outlook.com>
2022-08-14 20:33:25 +0100

    Modified structure to use OOP

--- hikeme_node_app/hikeme_node.py
+++ hikeme_node_app/hikeme_node.py
@@ -6,8 +6,44 @@ import sqlite3
 from django.conf import settings
 settings.configure(DEBUG=True)
 from django.contrib.auth.hashers import check_password
-from passlib.handlers.django import django_pbkdf2_sha256
 
+class MainWindow:
+    def __init__(self):
+        self.root = Tk()
+        self.root.geometry("1920x1080")
+        self.root.englandImage = Image.open("hikeme_node_app\england.jpg")
+        self.root.englandImage = ImageTk.PhotoImage(self.root.englandImage)
+        self.root.englandImageLabel = tkinter.Label(image=self.root.englandImage)
+        self.root.englandImageLabel.image = self.root.englandImage
+        self.root.pixel = tkinter.PhotoImage(width=5, height=5)
+        self.root.fileyButton=tkinter.Button(self.root, text="", image=self.root.pixel, bg='blue')
+        self.root.fileyButton.place(x=360, y=400)
+        self.root.saltburnButton=tkinter.Button(self.root, text="", image=self.root.pixel, bg='blue')
+        self.root.saltburnButton.place(x=325, y=375)
+        self.root.helmsleyButton=tkinter.Button(self.root, text="", image=self.root.pixel, bg='blue')
+        self.root.helmsleyButton.place(x=325, y=395)
+        self.root.mineheadButton=tkinter.Button(self.root, text="", image=self.root.pixel, bg='black')
+        self.root.mineheadButton.place(x=217, y=636)
+        self.root.stivesButton=tkinter.Button(self.root, text="", image=self.root.pixel, bg='black')
+        self.root.stivesButton.place(x=130, y=715)
+        self.root.brixhamButton=tkinter.Button(self.root, text="", image=self.root.pixel, bg='black')
+        self.root.brixhamButton.place(x=222, y=694)
+        self.root.pooleButton=tkinter.Button(self.root, text="", image=self.root.pixel, bg='black')
+        self.root.pooleButton.place(x=299, y=675)
+        self.root.doverButton=tkinter.Button(self.root, text="", image=self.root.pixel, bg='red')
+        self.root.doverButton.place(x=450, y=636)
+        self.root.kentButton=tkinter.Button(self.root, text="", image=self.root.pixel, bg='red')
+        self.root.kentButton.place(x=433, y=628)
+        self.root.otfordButton=tkinter.Button(self.root, text="", image=self.root.pixel, bg='red')
+        self.root.otfordButton.place(x=390, y=620)
+        self.root.farnhamButton=tkinter.Button(self.root, text="", image=self.root.pixel, bg='red')
+        self.root.farnhamButton.place(x=350, y=625)
+        self.root.loginButton=tkinter.Button(self.root, text="Login", command=lambda: loginWindow(self.root))
+        self.root.loginButton.place(bordermode=OUTSIDE)
+        self.root.englandImageLabel.place(x=0, y=0)
+
+    def run(self):
+        self.root.mainloop()
 
 def login(username, password):
     db = sqlite3.connect("hikeme_database.sqlite3")
@@ -26,10 +62,7 @@ def login(username, password):
     else:
         print("Username not found!")
 
-root = Tk()
-root.geometry("1920x1080")
-
-def loginWindow():
+def loginWindow(root):
     top = Toplevel(root)
     top.geometry("200x200")
 
@@ -48,44 +81,8 @@ def loginWindow():
 def closeWindow(top):
     top.destroy()
 
-# Create a photoimage object of the image in the path
-englandImage = Image.open("hikeme_node_app\england.jpg")
-englandImage = ImageTk.PhotoImage(englandImage)
+root = MainWindow()
+root.run()
 
-englandImageLabel = tkinter.Label(image=englandImage)
-englandImageLabel.image = englandImage
 
-pixel = tkinter.PhotoImage(width=5, height=5)
-fileyButton=tkinter.Button(root, text="", image=pixel, bg='blue')
-fileyButton.place(x=360, y=400)
-saltburnButton=tkinter.Button(root, text="", image=pixel, bg='blue')
-saltburnButton.place(x=325, y=375)
-helmsleyButton=tkinter.Button(root, text="", image=pixel, bg='blue')
-helmsleyButton.place(x=325, y=395)
-
-mineheadButton=tkinter.Button(root, text="", image=pixel, bg='black')
-mineheadButton.place(x=217, y=636)
-stivesButton=tkinter.Button(root, text="", image=pixel, bg='black')
-stivesButton.place(x=130, y=715)
-brixhamButton=tkinter.Button(root, text="", image=pixel, bg='black')
-brixhamButton.place(x=222, y=694)
-pooleButton=tkinter.Button(root, text="", image=pixel, bg='black')
-pooleButton.place(x=299, y=675)
-
-doverButton=tkinter.Button(root, text="", image=pixel, bg='red')
-doverButton.place(x=450, y=636)
-kentButton=tkinter.Button(root, text="", image=pixel, bg='red')
-kentButton.place(x=433, y=628)
-otfordButton=tkinter.Button(root, text="", image=pixel, bg='red')
-otfordButton.place(x=390, y=620)
-farnhamButton=tkinter.Button(root, text="", image=pixel, bg='red')
-farnhamButton.place(x=350, y=625)
-
-loginButton=tkinter.Button(root, text="Login", command=loginWindow)
-loginButton.place(bordermode=OUTSIDE)
-
-# Position image
-englandImageLabel.place(x=0, y=0)
-
-root.mainloop()
-
+ 
