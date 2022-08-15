f13e6f083747b87b7c8ebd2897f2da65b2337f06
cadeclark101 <cadeclark@outlook.com>
2022-08-15 17:51:56 +0100

    TODO: add function that loads UI for each window ratherthan loading directly in __init__ function

--- hikeme_node_app/control_menu.py
+++ hikeme_node_app/control_menu.py
@@ -11,19 +11,24 @@ class SuperuserControlMenuWindow(Toplevel):
         self.geometry("500x500")
         self.title("SUPERUSER Menu")
 
-        self.checkpointButtonIDList = self.mainapp.selectedCheckpoints
+        self.selectedCheckpoints = self.mainapp.selectedCheckpoints
+        self.updateSelectedCheckpoints()
 
-        if self.checkpointButtonIDList is None:
+    def updateSelectedCheckpoints(self):
+        self.listbox1 = Listbox(self)
+        self.listbox1.pack()
+
+        if self.selectedCheckpoints is None:
             self.label1 = Label(self, text="No checkpoints selected!").pack()
-            #self.listbox1 = Listbox(self)
-            #for i in range(len(self.checkpointButtonIDList)):
-                #self.listbox1.insert(i, str(self.checkpointButtonIDList[i]))
-            #self.listbox1.pack()
-        if len(self.checkpointButtonIDList) >= 3:
-            print(self.checkpointButtonIDList)
-            self.multithreadCB = Checkbutton(self, text='Enable Multithreading', onvalue=1, offvalue=0)
-            self.multithreadCB.pack()
-        
+        else:
+            for i in range(len(self.selectedCheckpoints)):
+                self.listbox1.insert(i, str(self.selectedCheckpoints[i]))
+            self.listbox1.pack()
+
+ #if len(self.checkpointButtonIDList) >= 3:
+    #print(self.checkpointButtonIDList)
+    #self.multithreadCB = Checkbutton(self, text='Enable Multithreading', onvalue=1, offvalue=0)
+    #self.multithreadCB.pack()       
 
 class ControlMenuWindow(Toplevel):
     def __init__(self, mainapp, master=None):
