0488e24e1cac587fa764f9cdefb1c81cb50480b3
cadeclark101 <cadeclark@outlook.com>
2022-08-15 14:31:53 +0100

    Figured out how to pass stuff between windows. Changed object structure.

--- hikeme_node_app/hikeme_node.py
+++ hikeme_node_app/hikeme_node.py
@@ -1,54 +1,65 @@
+from asyncio.windows_events import NULL
 import tkinter
 from tkinter import *
 from PIL import Image, ImageTk
 import login
+import os
 
 class MainWindow:
-    def __init__(self):
-        self.root = Tk()
-        self.root.geometry("1920x1080")
-        self.root.englandImage = Image.open("hikeme_node_app\england.jpg")
-        self.root.englandImage = ImageTk.PhotoImage(self.root.englandImage)
-        self.root.englandImageLabel = tkinter.Label(image=self.root.englandImage)
-        self.root.englandImageLabel.image = self.root.englandImage
+    def __init__(self, master):
+        self.master = master
+        self.master.title("Hikeme Node Network")
+        self.master.geometry("1920x1080")
 
-        self.root.pixel = tkinter.PhotoImage(width=5, height=5)
+        self.master.currentUser = None
 
-        self.root.fileyButton=tkinter.Button(self.root, text="", image=self.root.pixel, bg='blue')
-        self.root.fileyButton.place(x=360, y=400)
-        self.root.saltburnButton=tkinter.Button(self.root, text="", image=self.root.pixel, bg='blue')
-        self.root.saltburnButton.place(x=325, y=375)
-        self.root.helmsleyButton=tkinter.Button(self.root, text="", image=self.root.pixel, bg='blue')
-        self.root.helmsleyButton.place(x=325, y=395)
+        self.master.englandImage = Image.open("hikeme_node_app\england.jpg")
+        self.master.englandImage = ImageTk.PhotoImage(self.master.englandImage)
+        self.master.englandImageLabel = tkinter.Label(image=self.master.englandImage)
+        self.master.englandImageLabel.image = self.master.englandImage
 
-        self.root.mineheadButton=tkinter.Button(self.root, text="", image=self.root.pixel, bg='black')
-        self.root.mineheadButton.place(x=217, y=636)
-        self.root.stivesButton=tkinter.Button(self.root, text="", image=self.root.pixel, bg='black')
-        self.root.stivesButton.place(x=130, y=715)
-        self.root.brixhamButton=tkinter.Button(self.root, text="", image=self.root.pixel, bg='black')
-        self.root.brixhamButton.place(x=222, y=694)
-        self.root.pooleButton=tkinter.Button(self.root, text="", image=self.root.pixel, bg='black')
-        self.root.pooleButton.place(x=299, y=675)
+        self.master.pixel = tkinter.PhotoImage(width=5, height=5)
 
-        self.root.doverButton=tkinter.Button(self.root, text="", image=self.root.pixel, bg='red')
-        self.root.doverButton.place(x=450, y=636)
-        self.root.kentButton=tkinter.Button(self.root, text="", image=self.root.pixel, bg='red')
-        self.root.kentButton.place(x=433, y=628)
-        self.root.otfordButton=tkinter.Button(self.root, text="", image=self.root.pixel, bg='red')
-        self.root.otfordButton.place(x=390, y=620)
-        self.root.farnhamButton=tkinter.Button(self.root, text="", image=self.root.pixel, bg='red')
-        self.root.farnhamButton.place(x=350, y=625)
-        self.root.loginButton=tkinter.Button(self.root, text="Login", command=lambda: login.LoginWindow(self.root))
+        self.master.fileyButton=tkinter.Button(self.master, text="", image=self.master.pixel, bg='blue')
+        self.master.fileyButton.place(x=360, y=400)
+        self.master.saltburnButton=tkinter.Button(self.master, text="", image=self.master.pixel, bg='blue')
+        self.master.saltburnButton.place(x=325, y=375)
+        self.master.helmsleyButton=tkinter.Button(self.master, text="", image=self.master.pixel, bg='blue')
+        self.master.helmsleyButton.place(x=325, y=395)
 
-        self.root.loginButton.place(bordermode=OUTSIDE)
+        self.master.mineheadButton=tkinter.Button(self.master, text="", image=self.master.pixel, bg='black')
+        self.master.mineheadButton.place(x=217, y=636)
+        self.master.stivesButton=tkinter.Button(self.master, text="", image=self.master.pixel, bg='black')
+        self.master.stivesButton.place(x=130, y=715)
+        self.master.brixhamButton=tkinter.Button(self.master, text="", image=self.master.pixel, bg='black')
+        self.master.brixhamButton.place(x=222, y=694)
+        self.master.pooleButton=tkinter.Button(self.master, text="", image=self.master.pixel, bg='black')
+        self.master.pooleButton.place(x=299, y=675)
 
-        self.root.englandImageLabel.place(x=0, y=0)
+        self.master.doverButton=tkinter.Button(self.master, text="", image=self.master.pixel, bg='red')
+        self.master.doverButton.place(x=450, y=636)
+        self.master.kentButton=tkinter.Button(self.master, text="", image=self.master.pixel, bg='red')
+        self.master.kentButton.place(x=433, y=628)
+        self.master.otfordButton=tkinter.Button(self.master, text="", image=self.master.pixel, bg='red')
+        self.master.otfordButton.place(x=390, y=620)
+        self.master.farnhamButton=tkinter.Button(self.master, text="", image=self.master.pixel, bg='red')
+        self.master.farnhamButton.place(x=350, y=625)
+        
+        self.master.loginButton=tkinter.Button(self.master, text="Login", command=lambda: login.LoginWindow(self.master, self.master.currentUser))
+        self.master.loginButton.place(bordermode=OUTSIDE)
+
+        self.master.englandImageLabel.place(x=0, y=0)
 
     def run(self):
-        self.root.mainloop()
+        self.master.mainloop()
 
-root = MainWindow()
-root.run()
+def main():
+    os.system('cls')
+    root = Tk()
+    MainWindow(root)
+    root.mainloop()
+
+if __name__ == "__main__": main()
 
 
  
--- hikeme_node_app/login.py
+++ hikeme_node_app/login.py
@@ -1,3 +1,4 @@
+from hashlib import new
 import tkinter
 from tkinter import *
 import sqlite3
@@ -8,38 +9,42 @@ from django.contrib.auth.hashers import check_password
 db = sqlite3.connect("hikeme_database.sqlite3")
 cur = db.cursor()
 
-class LoginWindow:
-    def __init__(self, root):
-        self.loginWindow = Toplevel(root)
-        self.loginWindow.geometry("200x200")
+class LoginWindow(Toplevel):
+    def __init__(self, mainapp, master=None):
+        super().__init__(master=master)
+        self.mainapp = mainapp
 
-        self.loginWindow.usernameEntry = Entry(self.loginWindow, width = 25)
-        self.loginWindow.usernameEntry.pack()
-        self.loginWindow.passwordEntry = Entry(self.loginWindow, width = 25)
-        self.loginWindow.passwordEntry.pack()
-        self.loginWindow.submitButton=tkinter.Button(self.loginWindow, text="Submit", command=lambda: self.login(str(self.loginWindow.usernameEntry.get()), str(self.loginWindow.passwordEntry.get())))
-        self.loginWindow.submitButton.pack()
-
-        closeWindowButton= Button(self.loginWindow, text="Close", command=lambda: self.loginWindow.closeWindow)
-        closeWindowButton.pack(pady=5, side= TOP)
-
-    def run(self):
-        self.loginWindow.mainloop()
+        self.geometry("300x100")
+        self.title("Login")
+        
+        self.usernameEntry = Entry(self, width = 25)
+        self.usernameEntry.pack()
+        self.passwordEntry = Entry(self, width = 25)
+        self.passwordEntry.pack()
+        self.submitButton=tkinter.Button(self, text="Submit", command=lambda: self.login(str(self.usernameEntry.get()), str(self.passwordEntry.get())))
+        self.submitButton.pack()
+        self.label1 = Label(self, text="Try logging into a DJANGO Superuser.")
+        self.label1.pack(side=BOTTOM)
 
     def login(self, username, password):
-        query = f"SELECT username, password from auth_user WHERE username='{username}';"
+        query = f"SELECT username, password, is_superuser, id from auth_user WHERE username='{username}';"
         cur.execute(query)
         result = cur.fetchall()
-        grabbedUsername, grabbedPassword = result[[0][0]]
+        grabbedUsername, grabbedPassword, grabbedSuperuserStatus, grabbedID = result[[0][0]]
         
         if grabbedUsername is not None:
-            print("User: '" + username + "' found!")
             if check_password(password, grabbedPassword):
-                print("Passwords match!")
+                currentUser = CurrentUser(grabbedID, grabbedUsername, grabbedPassword, grabbedSuperuserStatus)
+                self.mainapp.currentUser = currentUser
+                self.destroy()
             else:
                 print("Wrong password!")
         else:
             print("Username not found!")
 
-    def closeWindow(self):
-        self.loginWindow.destroy()
+class CurrentUser:
+    def __init__(self, id, username, password, is_superuser):
+        self.userId = id
+        self.username = username
+        self.password = password
+        self.is_superuser = is_superuser 
