c12105a0a00992af5c6368fabc551c775ec624aa
cadeclark101 <cadeclark@outlook.com>
2022-09-14 21:11:16 +0100

    Added boostrap. Created navbar. Changed DB some more. Lots of planning

--- hikeme/settings.py
+++ hikeme/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 import sqlite3
 
@@ -117,13 +118,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "boot")
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGIN_REDIRECT_URL = "home"
-LOGOUT_REDIRECT_URL = "home"
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/"
 
--- hikeme_app/views.py
+++ hikeme_app/views.py
@@ -12,23 +12,33 @@ from django.http import HttpResponse
 
 class Home(View):
     def get(self, request):
-        self.current_user = request.user
-        self.current_person = get_object_or_404(Person, auth_user_id = self.current_user.id)
+        if request.user.is_authenticated:
+            self.current_user = request.user
+            self.current_person = get_object_or_404(Person, auth_user_id = self.current_user.id)
 
-        return render(request=request, template_name="home.html", 
-        context={"current_person":self.current_person, "current_trail":self.getCurrentUserTrail(), "current_trail_checkpoint":self.getCurrentUserTrailCheckpoint(), "all_trails":self.getAllTrails()})
+            return render(request=request, template_name="home.html", 
+            context={"current_person":self.current_person, "current_trail":self.getCurrentUserTrail(), "current_trail_checkpoint":self.getCurrentUserTrailCheckpoint(), "all_trails":self.getAllTrails()})
+        else:
+            return redirect("/accounts/login")
 
     def getCurrentUserTrail(self):
         current_trail = get_object_or_404(Trail, pk = self.current_person.current_trail_id)
         return current_trail
 
     def getCurrentUserTrailCheckpoint(self):
-        current_trail_checkpoint = get_object_or_404(Trail_Checkpoints, pk = self.current_person.current_trail_checkpoint_id)
+        current_trail_checkpoint = get_object_or_404(Trail_Checkpoint, pk = self.current_person.current_trail_checkpoint_id)
         return current_trail_checkpoint
 
     def getAllTrails(self):
         all_trails = Trail.objects.all()
         return all_trails
+
+    def getNewsFeed(self):
+        news_current_checkpoint = get_object_or_404(News, relevant_trail_checkpoint = self.current_person.current_trail_checkpoint_id)
+        news_current_trail = get_object_or_404(News, relevant_trail = self.current_person.current_trail_id)
+        return news_current_checkpoint, news_current_trail
+
+
 
 
 
